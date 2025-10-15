d02ecedbdb85808a09130ec0bcd8fc8f1f6d54bb
Szombati Olivér <o.szombati@gmail.com>
2025-10-15 15:21:37 +0200

    Initialized TypeORM

--- hangbank_server/src/app.module.ts
+++ hangbank_server/src/app.module.ts
@@ -3,11 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MinioModule } from './minio/minio.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true, //TODO: replace this with migrations in production
     }),
     MinioModule
   ],
